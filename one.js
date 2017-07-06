d0784ffa13edccbb70e209e027347197109ead4a
Sneha <puthenveedu_s@India.XoriantCorp.com>
2017-07-06 08:42:11 +0530

    my 15th commit

--- one.js
+++ one.js
@@ -1,5 +1,16 @@
 <!DOCTYPE html>
 <html>
+<head>
+<script>
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+</script>
+</head>
 <body>
 
 <h2>My First JavaScript</h2>
